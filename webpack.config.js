59f4468a9098e81c0afdbd8ee68d69af2ed5f923
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-03-31 19:11:29 +0500

    update cofig

--- webpack.config.js
+++ webpack.config.js
@@ -22,11 +22,6 @@ module.exports = {
       },
     ],
   },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
   optimization: {
     runtimeChunk: 'single',
   },
