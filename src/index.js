f6d3155c3ccd097fffe12e9559f1710f01b66ed7
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-01 16:21:39 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './style.css';
 
 document.querySelector('#add').onclick = function () {
-  if(document.querySelector('#task input').value.length == 0){
+  if(document.querySelector('#task input').value.length === 0){
     alert("Enter Task");
   }
   else{
       document.querySelector('#list').innerHTML += `
             <div class="tak">
-            <input type="checkbox" id="chkBox"/>
+            <input type="checkbox" id="chkBox">
                 <span id="taskname">
                     ${document.querySelector('#task input').value}
                 </span>
