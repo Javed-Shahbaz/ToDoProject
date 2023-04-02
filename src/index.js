eaef1dd093a1b9d1ecf7f5ccf387fb5918170b1d
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-02 10:39:49 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import './style.css';
 import {
-  tList, addTodo, delItem, // delCompleted, Chkbox,
-} from './Module/To-Do-List.js';
+  tList, addTodo, delItem,} from './Module/To-Do-List.js';
 
 window.addEventListener('load', () => {
   tList();
   addTodo();
-  // tList(Chkbox);
-  // delCompleted();
+  
 });
 tList(delItem);
