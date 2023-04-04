8a38bc9105b594bf32cc6bc645359cd9eb13e6fc
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-04-04 06:29:10 +0500

    update changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
 
-import {tList, addTodo, delItem, delCompleted, Chkbox}
-from './Module/To-Do-List.js';
+import { 
+  tList, addTodo, delItem, delCompleted, Chkbox 
+} from './Module/To-Do-List.js';
 
 window.addEventListener('load', () => {
   tList();
